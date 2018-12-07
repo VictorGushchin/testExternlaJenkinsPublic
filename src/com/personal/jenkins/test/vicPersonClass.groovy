bf378852c072383d73c4e6a1eba8468c77a230ad
vitek <victor_gushchin@yahoo.co.uk>
2018-12-07 12:18:22 +0100

    no message

---
+++ src/com/personal/jenkins/test/vicPersonClass.groovy
@@ -0,0 +1,13 @@
+package com.personal.jenkins.test
+
+class VicPersonConstructor implements Serializable
+{
+	String name
+	Integer age
+
+	VicPersonConstructor (name, age)
+	{
+		this.name=name
+		thias.age=age	
+	}
+}
